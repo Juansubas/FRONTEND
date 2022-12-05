66d395336c97ac9ee17c82aa2a4d2d75eabb2f50
Juansubas <juansubas@gmail.com>
2022-12-04 20:22:15 -0500

    Script module

--- Javascript-Import-Export/index.js
+++ Javascript-Import-Export/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+import {secret_number, add as sumar, subtract, divide} from './math.js'
+
+function add(a, b) {
+  return a + b
+}
+
+console.log(secret_number)
+console.log(sumar(100,20))
+console.log(add(100,20))
+console.log(subtract(100,20))
+console.log(divide(100,20))
