80b0a30469eda8a361a9ec10affb9d70a7685819
Juansubas <juansubas@gmail.com>
2022-12-04 21:55:58 -0500

    Backend - import - export - require

---
+++ Javascript-Import-Export/server/index.js
@@ -0,0 +1,8 @@
+// Forma sin modificar el package.json
+//const http = require("http")
+
+// Forma modificando el package Json
+import http from 'http'
+import {add} from './math.js'
+
+console.log(add(100,100))
