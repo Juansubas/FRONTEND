cc736141b5001188696df8d49a0bb8bd2d674f77
Juansubas <juansubas@gmail.com>
2023-01-19 18:33:11 -0500

    Creating Project with Typescript and Express

---
+++ ApiRest/src/routes/item.ts
@@ -0,0 +1,11 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+/**
+ * http://localhost:3002/items [GET]
+ */
+
+router.get("/items", (req: Request, res: Response) => {
+
+});
